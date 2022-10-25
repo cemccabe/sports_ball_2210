2c162bdd48d137e3833dabb8439988c2f589a6ea
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-25 14:26:18 -0600

    Create I3 methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,5 @@
+require './lib/player'
+
 class Team
   attr_reader :name, :city, :roster, :player_count
 
@@ -11,5 +13,38 @@ class Team
   def add_player(player)
     @roster << player
     @player_count += 1
+  end
+
+  def long_term_players
+    arr = []
+    @roster.each do |player|
+      if player.contract_length > 24
+        arr << player
+      end
+    end
+    arr
+  end
+
+  def short_term_players
+    arr = []
+    @roster.each do |player|
+      if player.contract_length <= 24
+        arr << player
+      end
+    end
+    arr
+  end
+
+  def total_value
+    total = 0
+    @roster.each do |player|
+      total += player.total_cost
+    end
+    total
+  end
+
+  def details
+    {"total_value" => total_value,
+    "player_count" => player_count}
   end
 end
